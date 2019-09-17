95cf0653c09cb9dda56de2bdf2dfd222eafe9831
Mihai Marginean <marginean.mihai@gmail.com>
2019-09-17 12:12:51 +0300

    Refactor

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import './card.styles.css';
+// import './card.styles.css';
+import {CardContainer} from './card.styles';
 class Card extends React.Component {
     render() {
         const monster = this.props.monster;
         const imgSrc = "https://robohash.org/" + monster.id;
         // console.log(monster);
         return (
-            <div className="monster">
+            <CardContainer>
                 <img src={imgSrc} /><br />
                 <span className="monsterName">{monster.name}</span><br />
                 <span className="monsterMail">{monster.email}</span>
                 
-            </div>
+            </CardContainer>
         );
     }
 }
---
+++ src/components/card/card.styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const CardContainer = styled.div`
+    display: inline-block;
+    width: 30%;
+    margin: 20px;
+    padding-bottom: 20px;
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    border-radius: 15px;
+    background: linear-gradient(to top, green, 30%, yellow);
+`;
+
+
+// .monster {
+//     display: inline-block;
+//     width: 30%;
+//     margin: 20px;
+//     padding-bottom: 20px;
+//     box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+//     transition: 0.3s;
+//     border-radius: 15px;
+//     background: linear-gradient(to top, green, 30%, yellow);
+//   }
+//   .monster:hover {
+//     box-shadow: 0 16px 32px 0 rgba(0,0,0,0.2);
+//     cursor: pointer;
+//   }
+//   .monsterName {
+//     font-size: 24px;
+//   }
