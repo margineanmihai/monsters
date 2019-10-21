1a525c3cb7670228f6ab671252edfd04a4f701f5
Mihai Marginean <marginean.mihai@gmail.com>
2019-10-21 16:24:15 +0300

    Updates

---
+++ precache-manifest.26538289dd99b64ae285a47cd9e73e8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485c20e0e8dbd9208413d229262803f0",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "e9d88d2089fd42a26652",
+    "url": "/monsters/static/css/main.0d0e0aac.chunk.css"
+  },
+  {
+    "revision": "7b20ccd4249a55271d6c",
+    "url": "/monsters/static/js/2.f3c17124.chunk.js"
+  },
+  {
+    "revision": "e9d88d2089fd42a26652",
+    "url": "/monsters/static/js/main.624897ef.chunk.js"
+  },
+  {
+    "revision": "82defad27a7103ef0112",
+    "url": "/monsters/static/js/runtime~main.800c269e.js"
+  }
+]);
